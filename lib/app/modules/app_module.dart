26d38c609eccc0ad324d2124bf96a9426eb35777
barretoalecio <aleciobarretu@gmail.com>
2024-01-14 13:59:56 -0300

    :sparkles: feat: Registered Coordinator Module in APP Module

--- lib/app/modules/app_module.dart
+++ lib/app/modules/app_module.dart
@@ -4,13 +4,29 @@ import 'package:http/http.dart' as http;
 
 import '../core/packages/network/http_client/http_client_implementation.dart';
 import '../core/packages/network/network_status/network_status.dart';
+import '../core/utils/app_routes/coordinator_module_routes.dart';
+import '../core/utils/extensions/guide_route_manage_extension.dart';
+import 'coordinator/coordinator_module.dart';
 
 class AppModule extends Module {
   @override
+  List<Module> get imports => [
+        CoordinatorModule(),
+      ];
+
+  @override
   void binds(Injector i) {
     i.add(http.Client.new);
-    i.add<http.Client>(HttpClientImplementation.new);
+    i.add(HttpClientImplementation.new);
     i.add(Connectivity.new);
     i.add(NetworkServiceImplementation.new);
   }
- }
+
+  @override
+  void routes(RouteManager r) {
+    r.guideModule(
+      CoordinatorModuleRoutes.moduleName,
+      module: CoordinatorModule(),
+    );
+  }
+}
